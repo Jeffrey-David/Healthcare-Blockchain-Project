788b46028164b075ed052d7b4323d394514920d8
Jeffrey-David <66271004+Jeffrey-David@users.noreply.github.com>
2024-04-01 17:54:48 +0800

    nothing

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,3 +1,8 @@
+// Load the 'dotenv' module to access environment variables
+const dotenv = require('dotenv').config({ path: '../../.env' });
+
+
+
 const DEHToken = artifacts.require("DEHToken");
 const TokenInitializer = artifacts.require("TokenInitializer");
 const MedicalRecordAccess = artifacts.require("MedicalRecordAccess");
@@ -5,23 +10,29 @@ const MedicalAppointment = artifacts.require("MedicalAppointment");
 
 module.exports = function (deployer, network, accounts) {
   // Deploy DEHToken contract
+  console.log("Hi" , process.env.MEDICALAPPOINTMENT_CONTRACT_ADDRESS)
+
   const ownerAddress = accounts[0];
   deployer.deploy(DEHToken).then(async () => {
     // Get the deployed instance of DEHToken contract
     const DEHTokenInstance = await DEHToken.deployed(); 
-    
+    process.env.DEHTOKEN_CONTRACT_ADDRESS = DEHTokenInstance.address;
+
     // Deploy TokenInitializer contract and pass DEHTokenInstance's address as argument
     await deployer.deploy(TokenInitializer, DEHTokenInstance.address);
-    
-    // Get the deployed instance of TokenInitializer contract
     const TokenInitializerInstance = await TokenInitializer.deployed();
-    
+    process.env.TOKENINITIALIZER_CONTRACT_ADDRESS = TokenInitializerInstance.address;
+
     // Deploy MedicalRecordAccess contract and pass TokenInitializerInstance's address as argument
     await deployer.deploy(MedicalRecordAccess);
-
-    const  MedicalRecordAccessInstance = await MedicalRecordAccess.deployed();
+    const MedicalRecordAccessInstance = await MedicalRecordAccess.deployed();
+    process.env.MEDICALRECORDACCESS_CONTRACT_ADDRESS = MedicalRecordAccessInstance.address;
     
     // Deploy MedicalAppointment contract and pass TokenInitializerInstance's address as argument
     await deployer.deploy(MedicalAppointment, ownerAddress, DEHTokenInstance.address, MedicalRecordAccessInstance.address);
+    const MedicalAppointmentInstance = await MedicalAppointment.deployed();
+    process.env.MEDICALAPPOINTMENT_CONTRACT_ADDRESS = MedicalAppointmentInstance.address;
+
+ 
   });
 };
